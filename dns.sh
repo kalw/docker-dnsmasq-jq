b9025deccdff2376116b5d7d2d323519b3dd65e8
Regis A. Despres <rdespres@digitas.fr>
2017-04-15 20:02:33 +0200

    add record capability

--- dns.sh
+++ dns.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -e
 
 DOMAIN_JSON=${DOMAIN_JSON:-'{}'}
+RECORD_JSON=${RECORD_JSON:-'{}'}
 # by default send queries to all servers that can reply
 DNSMASQ_OPTIONS=${DNSMASQ_OPTIONS:---all-servers}
 
-function configure() {
+function configure_domain() {
   cat << EOF > /etc/dnsmasq.conf
 user=root
 $(jq '. as $servers|keys[]|. as $domain|$servers[$domain][]|"server=/"+$domain+"/"+.' -r)
+EOF
+}
+
+function configure_record() {
+  cat << EOF >> /etc/dnsmasq.conf
+user=root
+$(jq '. as $servers|keys[]|. as $domain|$servers[$domain][]|"address=/"+$domain+"/"+.' -r)
 EOF
 }
 
@@ -15,5 +23,6 @@ function run() {
   exec dnsmasq -k $DNSMASQ_OPTIONS
 }
 
-echo $DOMAIN_JSON | configure
+echo $DOMAIN_JSON | configure_domain
+echo $RECORD_JSON | configure_record
 run
